dd57b2a50a1f7d3d01304cf8e29c9a7caa162140
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-17 13:14:17 -0700

    create string_fields.c

---
+++ string_fields.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_precision - function to get the precision
+ * @b: string
+ * @params: parameters
+ * @az: the argument pointer
+ * Return: a pointer
+ */
+
+char *get_precision(char *b, params_t *params, va_list az)
+{
+	int x = 0;
+
+	if (*b != '.')
+		return (b);
+	b++;
+	if (*b == '*')
+	{
+		x = va_arg(az, int);
+		b++;
+	}
+	else
+	{
+		while (_isdigit(*b))
+			x = x * 10 + (*b++ - '0');
+	}
+	params->precision = x;
+	return (b);
+}
