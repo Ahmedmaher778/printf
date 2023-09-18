2e6664c18f4cd25f20147bb970dbc3d0ab3b872f
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-09-18 12:59:22 -0700

    params.c

--- params.c
+++ params.c
@@ -3,26 +3,26 @@
 /**
  * init_params - clears struct fields and reset buf
  * @params: parameters struct
- * @az: argument pointer
+ * @ap: argument pointer
  *
  * Return: void
 */
 
-void init_params(params_t *params, va_list az)
+void init_params(params_t *params, va_list ap)
 {
-	params->unsgn = 0;
+	params->unsign = 0;
 
-	params->pl_flag = 0;
-	params->sp_flag = 0;
-	params->hash_flag = 0;
-	params->z_flag = 0;
-	params->m_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
 
-	params->width = 0;
 	params->precision = UINT_MAX;
+	params->width = 0;
 
-	params->h_modifier = 0;
 	params->l_modifier = 0;
-	(void)az;
+	params->h_modifier = 0;
+	(void)ap;
 }
 
