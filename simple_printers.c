5cf243b3fe1dcf5e16ba9e7e21dbce1e7ba32533
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-18 14:24:51 -0700

    edit simple_printers.c file

--- simple_printers.c
+++ simple_printers.c
@@ -22,14 +22,14 @@ int print_from_to(char *start, char *stop, char *except)
 
 /**
  * print_rev - function prints in reverse string
- * @az: is a string
+ * @ap: is a string
  * @params: the parameters
  * Return: the number of bytes printed
  */
-int print_rev(va_list az, params_t *params)
+int print_rev(va_list ap, params_t *params)
 {
 	int length, summation = 0;
-	char *string = va_arg(az, char *);
+	char *string = va_arg(ap, char *);
 	(void)params;
 
 	if (string)
@@ -45,27 +45,28 @@ int print_rev(va_list az, params_t *params)
 
 /**
  * print_rot13 - function prints string
- * @az: is a string
+ * @ap: is a string
  * @params: the parameters
  * Return: the number of bytes printed
  */
-int print_rot13(va_list az, params_t *params)
+int print_rot13(va_list ap, params_t *params)
 {
-	int x, index, counter = 0;
+	int x, i;
+	int counter = 0;
 	char array[] =
 		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
-	char *a = va_arg(az, char *);
+	char *a = va_arg(ap, char *);
 	(void)params;
 
-	index = 0;
 	x = 0;
+	i = 0;
 	while (a[x])
 	{
 		if ((a[x] >= 'A' && a[x] <= 'Z')
 		    || (a[x] >= 'a' && a[x] <= 'z'))
 		{
-			index = a[x] - 65;
-			counter += _putchar(array[index]);
+			i = a[x] - 65;
+			counter += _putchar(array[i]);
 		}
 		else
 			counter += _putchar(a[x]);
