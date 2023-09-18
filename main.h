25110507c6dd611ec86c620f6049a8ccbbf083c6
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-09-18 14:40:11 -0700

    main.h

--- main.h
+++ main.h
@@ -36,13 +36,13 @@
 
 typedef struct parameters
 {
-	unsigned int unsign			: 1;
+	unsigned int unsign		: 1;
 
 	unsigned int zero_flag		: 1;
 	unsigned int space_flag		: 1;
-	unsigned int minus_flag	: 1;
+	unsigned int minus_flag		: 1;
 	unsigned int plus_flag		: 1;
-	unsigned int hashtag_flag		: 1;
+	unsigned int hashtag_flag	: 1;
 
 	unsigned int precision;
 	unsigned int width;
