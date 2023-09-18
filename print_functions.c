12480e27987e53c2906355687156e22abc465613
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-18 14:18:48 -0700

    edit print_functions.c file

--- print_functions.c
+++ print_functions.c
@@ -2,106 +2,106 @@
 
 /**
  * print_char - function prints character
- * @az: the pointer argument
+ * @ap: the pointer argument
  * @params: the parameters
  * Return: the printed number chars
  */
-int print_char(va_list az, params_t *params)
+int print_char(va_list ap, params_t *params)
 {
 	char pad_character = ' ';
-	unsigned int pad_number = 1, summation = 0, character = va_arg(az, int);
+	unsigned int pad_number = 1, summation = 0, ch = va_arg(ap, int);
 
-	if (params->m_flag)
-		summation += _putchar(character);
+	if (params->minus_flag)
+		summation += _putchar(ch);
 	while (pad_number++ < params->width)
 		summation += _putchar(pad_character);
-	if (!params->m_flag)
-		summation += _putchar(character);
+	if (!params->minus_flag)
+		summation += _putchar(ch);
 	return (summation);
 }
 
 /**
  * print_int - function prints integer
- * @az: the pointer argument
+ * @ap: the pointer argument
  * @params: the parameters
  * Return: the printed number chars
  */
-int print_int(va_list az, params_t *params)
+int print_int(va_list ap, params_t *params)
 {
-	long len;
+	long longLen;
 
 	if (params->l_modifier)
-		len = va_arg(az, long);
+		longLen = va_arg(ap, long);
 	else if (params->h_modifier)
-		len = (short int)va_arg(az, int);
+		longLen = (short int)va_arg(ap, int);
 	else
-		len = (int)va_arg(az, int);
-	return (print_number(convert(len, 10, 0, params), params));
+		longLen = (int)va_arg(ap, int);
+	return (print_number(convert(longLen, 10, 0, params), params));
 }
 
 /**
  * print_string - function prints string
- * @az: the pointer argument
+ * @ap: the pointer argument
  * @params: the parameters
  * Return: the printed number chars
  */
-int print_string(va_list az, params_t *params)
+int print_string(va_list ap, params_t *params)
 {
-	char *string = va_arg(az, char *), pad_character = ' ';
+	char **string = va_arg(ap, char *), pad_character = ' ';
 	unsigned int pad = 0, summation = 0, x = 0, y;
 
 	(void)params;
-	switch ((int)(!string))
+	switch ((int)(!*string))
 		case 1:
-			string = NULL_STRING;
+			*string = NULL_STRING;
 
-	y = pad = _strlen(string);
+	y = pad = _strlen(*string);
 	if (params->precision < pad)
 		y = pad = params->precision;
 
-	if (params->m_flag)
+	if (params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
 			for (x = 0; x < pad; x++)
-				summation += _putchar(*string++);
+				summation += _putchar(**string++);
 		else
-			summation += _puts(string);
+			summation += _puts(*string);
 	}
 	while (y++ < params->width)
 		summation += _putchar(pad_character);
-	if (!params->m_flag)
+	if (!params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
 			for (x = 0; x < pad; x++)
-				summation += _putchar(*string++);
+				summation += _putchar(**string++);
 		else
-			summation += _puts(string);
+			summation += _puts(*string);
 	}
 	return (summation);
 }
 
 /**
  * print_percent - the prints string
- * @az: the pointer argument
+ * @ap: the pointer argument
  * @params: the parameters
  * Return: the printed number chars
  */
-int print_percent(va_list az, params_t *params)
+int print_percent(va_list ap, params_t *params)
 {
-	(void)az;
+	(void)ap;
 	(void)params;
 	return (_putchar('%'));
 }
 
 /**
  * print_S - function that make a custom format specifier
- * @az: the pointer argument
+ * @ap: the pointer argument
  * @params: the parameters
  * Return: the printed number chars
  */
-int print_S(va_list az, params_t *params)
+int print_S(va_list ap, params_t *params)
 {
-	char *string = va_arg(az, char *);
+	char *string = va_arg(ap, char *);
 	char *hex;
 	int summation = 0;
 
