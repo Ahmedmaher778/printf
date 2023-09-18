5b72bbf3be2e01dd269f7044c47ddf6eac20f39f
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-18 00:44:49 -0700

    create print_functions.c file

---
+++ print_functions.c
@@ -0,0 +1,127 @@
+#include "main.h"
+
+/**
+ * print_char - function prints character
+ * @az: the pointer argument
+ * @params: the parameters
+ * Return: the printed number chars
+ */
+int print_char(va_list az, params_r *params)
+{
+	char pad_character = ' ';
+	unsigned int pad_number = 1, summation = 0, character = va_arg(az, int);
+
+	if (params->m_flag)
+		summation += _putchar(character);
+	while (pad_number++ < params->wid)
+		summation += _putchar(pad_character);
+	if (!params->m_flag)
+		summation += _putchar(character);
+	return (summation);
+}
+
+/**
+ * print_int - function prints integer
+ * @az: the pointer argument
+ * @params: the parameters
+ * Return: the printed number chars
+ */
+int print_int(va_list az, params_r *params)
+{
+	long len;
+
+	if (params->j_modifier)
+		len = va_arg(az, long);
+	else if (params->g_modifier)
+		len = (short int)va_arg(az, int);
+	else
+		len = (int)va_arg(az, int);
+	return (print_number(convert(len, 10, 0, params), params));
+}
+
+/**
+ * print_string - function prints string
+ * @az: the pointer argument
+ * @params: the parameters
+ * Return: the printed number chars
+ */
+int print_string(va_list az, params_r *params)
+{
+	char *string = va_arg(az, char *), pad_character = ' ';
+	unsigned int pad = 0, summation = 0, x = 0, y;
+
+	(void)params;
+	switch ((int)(!string))
+		case 1:
+			string = NULL_STRING;
+
+	y = pad = _strlen(string);
+	if (params->precision < pad)
+		y = pad = params->precision;
+
+	if (params->m_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (x = 0; x < pad; x++)
+				summation += _putchar(*string++);
+		else
+			summation += _puts(string);
+	}
+	while (y++ < params->wid)
+		summation += _putchar(pad_character);
+	if (!params->m_flag)
+	{
+		if (params->precision != UINT_MAX)
+			for (x = 0; x < pad; x++)
+				summation += _putchar(*string++);
+		else
+			summation += _puts(string);
+	}
+	return (summation);
+}
+
+/**
+ * print_percent - the prints string
+ * @az: the pointer argument
+ * @params: the parameters
+ * Return: the printed number chars
+ */
+int print_percent(va_list az, params_r *params)
+{
+	(void)az;
+	(void)params;
+	return (_putchar('%'));
+}
+
+/**
+ * print_S - function that make a custom format specifier
+ * @az: the pointer argument
+ * @params: the parameters
+ * Return: the printed number chars
+ */
+int print_S(va_list az, params_r *params)
+{
+	char *string = va_arg(az, char *);
+	char *hex;
+	int summation = 0;
+
+	if ((int)(!string))
+		return (_puts(NULL_STRING));
+	for (; *string; string++)
+	{
+		if ((*string > 0 && *string < 32) || *string >= 127)
+		{
+			summation += _putchar('\\');
+			summation += _putchar('x');
+			hex = convert(*string, 16, 0, params);
+			if (!hex[1])
+				summation += _putchar('0');
+			summation += _puts(hex);
+		}
+		else
+		{
+			summation += _putchar(*string);
+		}
+	}
+	return (summation);
+}
