2edb5f56b6e1b0fcee4a25617b23fc53a8894bf5
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-18 14:27:22 -0700

    edit string_fields.c file

--- string_fields.c
+++ string_fields.c
@@ -4,20 +4,21 @@
  * get_precision - function to get the precision
  * @b: string
  * @params: parameters
- * @az: the argument pointer
+ * @ap: the argument pointer
  * Return: a pointer
  */
-
-char *get_precision(char *p, params_t *params, va_list az)
+char *get_precision(char *p, params_t *params, va_list ap)
 {
 	int x = 0;
 
 	if (*p != '.')
+	{
 		return (p);
+	}
 	p++;
 	if (*p == '*')
 	{
-		x = va_arg(az, int);
+		x = va_arg(ap, int);
 		p++;
 	}
 	else
