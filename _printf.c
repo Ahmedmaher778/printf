60b54fa23b89ef951821a55dabca2ff4b138eaa6
mahmoudkandel980 <mahmoudkandel980@gmail.com>
2023-09-18 13:37:28 -0700

    edit _printf.c file

--- _printf.c
+++ _printf.c
@@ -5,15 +5,14 @@
  * @format: format string
  * Return: number of bytes printed
  */
-
 int _printf(const char *format, ...)
 {
 	int summation = 0;
-	char *p, *beginning;
-	va_list az;
+	va_list ap;
+	char *p, *start;
 	params_t params = PARAMS_INIT;
 
-	va_start(az, format);
+	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
@@ -21,29 +20,29 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-		init_params(&params, az);
+		init_params(&params, ap);
 		if (*p != '%')
 		{
 			summation += _putchar(*p);
 			continue;
 		}
-		beginning = p;
+		start = p;
 		p++;
 		while (get_flag(p, &params))
 		{
 			p++;
 		}
-		p = get_width(p, &params, az);
-		p = get_precision(p, &params, az);
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
-			summation += print_from_to(beginning, p,
+			summation += print_from_to(start, p,
 				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			summation += get_print_func(p, az, &params);
+			summation += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
-	va_end(az);
+	va_end(ap);
 	return (summation);
 }
