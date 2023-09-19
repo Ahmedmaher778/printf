23b3a014d398a6efdf3af46d55981674293cd196
Mahmoud Korim Kandel <mahmoudkandel980@gmail.com>
2023-09-19 03:37:12 +0300

    Update number.c

--- number.c
+++ number.c
@@ -2,8 +2,10 @@
 
 /**
  * convert - function prints integer unsigned numbers
- * @ap: pointer argument
- * @params: the parameters
+ * @num: the number
+ * @base: the base
+ * @flags: the flags argument
+ * @params: the paramater
  * Return: printed bytes
  */
 char *convert(long int num, int base, int flags, params_t *params)
