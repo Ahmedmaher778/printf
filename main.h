2298c8d25c1afa453f7e809e266e804b1c5e08cc
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-09-19 17:11:56 -0700

    r

--- main.h
+++ main.h
@@ -7,50 +7,49 @@
 #include <limits.h>
 #include <stdlib.h>
 
-#define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
-
+#define OUTPUT_BUF_SIZE 1024
 
 #define NULL_STRING "(null)"
 
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
-#define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
+#define CONVERT_LOWERCASE	1
 
 /**
  * struct parameters - parameters struct
  *
  * @unsign: flag if unsigned value
  *
- * @plus_flag: on if plus_flag specified
- * @space_flag: on if hashtag_flag specified
- * @hashtag_flag: on if _flag specified
- * @zero_flag: on if _flag specified
- * @minus_flag: on if _flag specified
- *
- * @width: field width specified
- * @precision: field precision specified
- *
- * @h_modifier: on if h_modifier is specified
  * @l_modifier: on if l_modifier is specified
+ * @h_modifier: on if h_modifier is specified
+ *
+ * @precision: field precision specified
+ * @width: field width specified
+ *
+ * @space_flag: on if hashtag_flag specified
+ * @plus_flag: on if plus_flag specified
+ * @zero_flag: on if _flag specified
+ * @hashtag_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
  *
  */
 typedef struct parameters
 {
 	unsigned int unsign			: 1;
 
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
+	unsigned int l_modifier		: 1;
+	unsigned int h_modifier		: 1;
 
 	unsigned int width;
 	unsigned int precision;
 
-	unsigned int h_modifier		: 1;
-	unsigned int l_modifier		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int plus_flag		: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int minus_flag		: 1;
 } params_t;
 
 /**
@@ -66,45 +65,45 @@ typedef struct specifier
 } specifier_t;
 
 /* _put.c module */
-int _puts(char *str);
 int _putchar(int c);
+int _puts(char *str);
 
 /* print_functions.c module */
-int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
+int print_char(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
 int print_S(va_list ap, params_t *params);
 
 /* number.c module */
-char *convert(long int num, int base, int flags, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
 int print_address(va_list ap, params_t *params);
+char *convert(long int num, int base, int flags, params_t *params);
 
 /* specifier.c module */
-int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_print_func(char *s, va_list ap, params_t *params);
-int get_flag(char *s, params_t *params);
+int (*get_specifier(char *s))(va_list ap, params_t *params);
 int get_modifier(char *s, params_t *params);
 char *get_width(char *s, params_t *params, va_list ap);
+int get_flag(char *s, params_t *params);
 
 /* convert_number.c module */
-int print_hex(va_list ap, params_t *params);
 int print_HEX(va_list ap, params_t *params);
-int print_binary(va_list ap, params_t *params);
+int print_hex(va_list ap, params_t *params);
 int print_octal(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
 
 /* simple_printers.c module */
-int print_from_to(char *start, char *stop, char *except);
 int print_rev(va_list ap, params_t *params);
 int print_rot13(va_list ap, params_t *params);
+int print_from_to(char *start, char *stop, char *except);
 
 /* print_number.c module */
-int _isdigit(int c);
 int _strlen(char *s);
-int print_number(char *str, params_t *params);
+int _isdigit(int c);
 int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
+int print_number(char *str, params_t *params);
 
 /* params.c module */
 void init_params(params_t *params, va_list ap);
